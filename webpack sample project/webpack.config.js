9227eb2b9360ae06d7274918391c97ea33ce9391
吕江腾 <lvjiangteng@126.com>
2019-04-13 23:27:53 +0800

    使用HtmlWebpackPlugin插件自动生成html

--- webpack sample project/webpack.config.js
+++ webpack sample project/webpack.config.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     devtool: "eval-source-map",
     entry: __dirname + "/app/main.js",
     output: {
-        path: __dirname + "/public",
+        path: __dirname + "/build",
         filename: "bundle.js"
     },
     devServer: {
@@ -38,6 +40,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.BannerPlugin('版权所有，翻版必究')
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+        })
     ],
 }
