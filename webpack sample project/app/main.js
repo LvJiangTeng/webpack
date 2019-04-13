d9d5af376fe38525efad2f3a22c2b36d0238ae72
吕江腾 <lvjiangteng@126.com>
2019-04-13 23:23:51 +0800

    使用插件  后面会用html-plugin

--- webpack sample project/app/main.js
+++ webpack sample project/app/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Greeter from './Greeter';
-
+import Greeter from './Greeting';
+import './main.css'
 render(<Greeter />, document.getElementById('root'));
--- webpack sample project/webpack.config.js
+++ webpack sample project/webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 module.exports = {
     devtool: "eval-source-map",
     entry: __dirname + "/app/main.js",
@@ -15,15 +16,28 @@ module.exports = {
             {
                 test: /(\.jsx|\.js)$/,
                 use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "env", "react"
-                        ]
-                    }
+                    loader: "babel-loader"
                 },
                 exclude: /node_modules/
+            }, {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader",
+                        options: {
+                            modules: true, // 指定启用css modules
+                            localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+                        }
+                    }, {
+                        loader: "postcss-loader"
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究')
+    ],
 }
